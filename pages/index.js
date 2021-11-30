765e19517a758f671b9e7c6f3d37a0cccf984112
Moisés Lopes Ferreira <moiseslopesferreira@outlook.com>
2021-11-29 22:58:55 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>D'Gust App</title>
+        <title>D&aposGust App</title>
         <meta name="description" content="Inovação financeira para restaurantes. Gerir o seu restaurante nunca foi tão acessível." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          {`Bem Vindo Ao D'Gust APP`}
+          Bem Vindo Ao D&aposGust APP
         </h1>
         <p className={styles.description}>Inovação financeira para restaurantes. Gerir o seu restaurante nunca foi tão acessível.</p>
       </main>
