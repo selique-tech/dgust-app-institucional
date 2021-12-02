ecf8fae8bc661073d3bb3400f122a4da7c57e7e0
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2021-12-01 23:33:21 -0300

    Add logo and desc

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>D&aposGust App</title>
-        <meta name="description" content="Inovação financeira para restaurantes. Gerir o seu restaurante nunca foi tão acessível." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>{`D'Gust App`}</title>
+				<meta
+					name="description"
+					content="Inovação financeira para restaurantes. Gerir o seu restaurante nunca foi tão acessível."
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Bem Vindo Ao D&aposGust APP
-        </h1>
-        <p className={styles.description}>Inovação financeira para restaurantes. Gerir o seu restaurante nunca foi tão acessível.</p>
-      </main>
-    </div>
-  )
+			<main className={styles.main}>
+				<p className={styles.description}>
+					{`Em breve, a D'Gust App estará disponível para Web, Android e iOS.`}
+				</p>
+				<Image
+					src="/logo.png"
+					width="300%"
+					height="300%"
+					objectFit="contain"
+					alt="D'Gust App"
+				/>
+				<h1 className={styles.title}>{`D'Gust APP`}</h1>
+				<p className={styles.description}>
+					{`Inovação financeira para restaurantes. Gerir o seu
+					restaurante nunca foi tão acessível.`}
+				</p>
+			</main>
+		</div>
+	);
 }
